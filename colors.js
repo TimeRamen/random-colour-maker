78037ea0b6d49134bd1259216f6579413a192306
Taimur Rahman <taimurrahman98@gmail.com>
2018-09-10 14:01:45 +0200

    basic app completed
    white font appears when changing colour, smarter button input that will allow for more buttons in the future.
    slight css to make it look a bit better

--- colors.js
+++ colors.js
@@ -3,8 +3,21 @@ var helpButton = document.querySelector(".question");
 var toggleButton = document.querySelector(".toggler");
 var helpDisplay = document.querySelector(".help");
 var colourDisplay = document.querySelector(".colours");
+var rgbDisplay = document.querySelector(".colours span#rgb-colour");
+var hexDisplay = document.querySelector(".colours span#hex-colour");
 var advancedTools = document.querySelectorAll(".advance-tips");
 var colourButtons = document.querySelectorAll("button");
+/* From http://www.javascripter.net/faq/rgbtohex.htm */
+function rgbToHex(R, G, B) { return toHex(R) + toHex(G) + toHex(B); }
+function toHex(n) {
+    n = parseInt(n, 10);
+    if (isNaN(n))
+        return "00";
+    n = Math.max(0, Math.min(n, 255));
+    return "0123456789ABCDEF".charAt((n - n % 16) / 16)
+        + "0123456789ABCDEF".charAt(n % 16);
+}
+/* -------------- */
 helpButton.addEventListener("click", function () {
     helpDisplay.classList.toggle("hidden");
     colourDisplay.classList.toggle("hidden");
@@ -22,22 +35,51 @@ function showAdvanceTools() {
 function returnRandom(start, length) {
     return Math.floor(Math.random() * length) + start;
 }
-function randomColourChannel() {
-    return returnRandom(0, 255);
+function buttonRandomColourGenerator(buttonID) {
+    if (buttonID === "random") {
+        return returnRandom(0, 255);
+    }
+    else if (buttonID === "light") {
+        return returnRandom(200, 55);
+    }
+    else if (buttonID === "dark") {
+        return returnRandom(0, 55);
+    }
+    else {
+        return returnRandom(0, 255);
+    }
 }
 function initialiseButtons() {
     for (var i = 0; i < colourButtons.length; i++) {
         colourButtons[i].addEventListener("click", function () {
-            background.style.backgroundColor = randomColour();
+            background.style.backgroundColor = randomColour(this.id);
         });
     }
 }
-function randomColour() {
-    var colourString = "rgb(" + randomColourChannel() + "," + randomColourChannel() + "," + randomColourChannel() + ")";
-    return colourString;
+function randomColour(buttonID) {
+    var r = buttonRandomColourGenerator(buttonID);
+    var g = buttonRandomColourGenerator(buttonID);
+    var b = buttonRandomColourGenerator(buttonID);
+    changeFontColour(r, g, b);
+    updateRGBDisplay(r, g, b);
+    return returnColourString(r, g, b);
 }
-function setColour() {
-    return "rgb(100,200,30)";
+function returnColourString(r, g, b) {
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
+function updateRGBDisplay(r, g, b) {
+    rgbDisplay.textContent = r + "," + g + "," + b;
+    hexDisplay.textContent = rgbToHex(r, g, b);
+}
+function changeFontColour(r, g, b) {
+    var allUnder128 = r < 128 && g < 128 && b < 128;
+    var twoUnder60 = (r < 60 && g < 60) || (b < 60 && g < 60) || (r < 60 && b < 60);
+    if (allUnder128 || twoUnder60) {
+        background.style.color = "white";
+    }
+    else {
+        background.style.color = "black";
+    }
 }
 function initialiseApp() {
     initialiseButtons();
--- colors.ts
+++ colors.ts
@@ -3,8 +3,21 @@ var helpButton: HTMLElement = document.querySelector(".question");
 var toggleButton: HTMLElement = document.querySelector(".toggler");
 var helpDisplay: HTMLElement = document.querySelector(".help");
 var colourDisplay: HTMLElement = document.querySelector(".colours");
+var rgbDisplay: HTMLElement = document.querySelector(".colours span#rgb-colour");
+var hexDisplay: HTMLElement = document.querySelector(".colours span#hex-colour");
 var advancedTools: NodeListOf<HTMLElement> = document.querySelectorAll(".advance-tips");
 var colourButtons: NodeListOf<HTMLElement> = document.querySelectorAll("button");
+
+/* From http://www.javascripter.net/faq/rgbtohex.htm */
+function rgbToHex(R: number,G: number,B: number) {return toHex(R)+toHex(G)+toHex(B)}
+function toHex(n: number) {
+ n = parseInt(n,10);
+ if (isNaN(n)) return "00";
+ n = Math.max(0,Math.min(n,255));
+ return "0123456789ABCDEF".charAt((n-n%16)/16)
+      + "0123456789ABCDEF".charAt(n%16);
+}
+/* -------------- */
 
 
 helpButton.addEventListener("click",function(): void{
@@ -28,25 +41,52 @@ function returnRandom(start: number, length: number): number{
     return Math.floor(Math.random() * length) + start;
 }
 
-function randomColourChannel(): number{
-    return returnRandom(0,255);
+function buttonRandomColourGenerator(buttonID: string): number{
+    if(buttonID === "random"){
+        return returnRandom(0,255);
+    }
+    else if(buttonID === "light"){
+        return returnRandom(200,55);
+    }
+    else if(buttonID === "dark"){
+        return returnRandom(0,55);
+    }else{
+        return returnRandom(0,255);
+    }
 }
 
 function initialiseButtons(): void{
     for(let i: number = 0; i < colourButtons.length ; i++){
         colourButtons[i].addEventListener("click",function(){
-            background.style.backgroundColor = randomColour();
+            background.style.backgroundColor = randomColour(this.id);
         })
     }
 }
-function randomColour(): string{
-    let colourString: string = "rgb(" + randomColourChannel() + "," + randomColourChannel() + "," + randomColourChannel() + ")";
-    return colourString;
+function randomColour(buttonID: string): string{
+    let r = buttonRandomColourGenerator(buttonID);
+    let g = buttonRandomColourGenerator(buttonID);
+    let b = buttonRandomColourGenerator(buttonID);
+    changeFontColour(r,g,b);
+    updateRGBDisplay(r,g,b);
+    return returnColourString(r,g,b);
 }
-function setColour(): string{
-    return "rgb(100,200,30)";
+function returnColourString(r: number, g:number, b:number): string{
+    return "rgb(" + r + "," + g + "," + b + ")";
 }
-
+function updateRGBDisplay(r: number, g:number, b:number): void{
+    rgbDisplay.textContent = r + "," + g + "," + b;
+    hexDisplay.textContent = rgbToHex(r,g,b);
+}
+function changeFontColour(r: number, g:number, b:number): void{
+    let allUnder128 : boolean = r < 128 && g < 128 && b < 128;
+    let twoUnder60: boolean = (r < 60 && g < 60) || (b < 60 && g < 60) || (r < 60 && b < 60);
+    if(allUnder128 || twoUnder60){
+        background.style.color = "white";
+    }
+    else{
+        background.style.color = "black";
+    }
+}
 function initialiseApp(): void{
     initialiseButtons();
 }
